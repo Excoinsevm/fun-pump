f37dce0c67bf6086767f0319b4971cd1c0e92ea3
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-18 01:13:03 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,3 @@
-"use client"
-
 import { Nabla } from "next/font/google";
 import { WagmiConfig } from "wagmi";
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
@@ -8,11 +6,6 @@ import "@rainbow-me/rainbowkit/styles.css"; // Import RainbowKit styles
 import "./globals.css";
 
 const nabla = Nabla({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "fun.pump",
-  description: "create token listings",
-};
 
 export default function RootLayout({ children }) {
   return (
